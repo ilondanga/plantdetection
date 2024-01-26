2bfa5855079a5e41cc6258666e901bbade8143cf
patricia vakola <91668122+ilondanga@users.noreply.github.com>
2024-01-26 15:40:44 +0300

    Update samples.py

--- samples.py
+++ samples.py
@@ -1,9 +1,1 @@
-employee = []
-employee.append ('jane')
-employee.append ('jacky')
-employee.append ('lucy')
-print ('employees:',employee)
 
-class Employee(object):
-    def CreateEmployee(self):
-        pass
